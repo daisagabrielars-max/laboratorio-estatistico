01f0b694fe376bbd0fd7ef191edf6bc5825ad272
Daísa Gabriela <daisagabriela29@gmail.com>
2026-08-18 17:16:45 -0300

    Adiciona funcao moda com tratamento bimodal e amodal

--- minhastats.py
+++ minhastats.py
@@ -9,3 +9,16 @@ def mediana(dados):
         return  (ordenados[n // 2 - 1] + ordenados[n // 2]) / 2
 def amplitude(dados):
     return max(dados) - min(dados)   
+def moda(dados):
+    contagem = {}
+    for v in dados:
+        if v in contagem:
+            contagem[v] = contagem[v] + 1
+        else:
+            contagem[v] = 1
+    maior = max(contagem.values())
+    modas = []
+    for valor in contagem:
+        if contagem[valor] == maior:
+            modas.append(valor)
+    return modas
